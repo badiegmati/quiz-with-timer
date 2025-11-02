06cf301386b0209723b1b2ff5729b62f3b04d7a2
badie <badiegmati11@gmail.com>
2025-11-02 21:07:20 +0100

    Description des modifications temp 11

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,7 +6,7 @@ const Quiz = ({ questions, studentName, onComplete }) => {
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [answers, setAnswers] = useState([]);
   const [showFeedback, setShowFeedback] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(31);
+  const [timeLeft, setTimeLeft] = useState(11);
   const [answerTime, setAnswerTime] = useState(0);
 
   const currentQuestion = questions[currentQuestionIndex];
@@ -32,7 +32,7 @@ const Quiz = ({ questions, studentName, onComplete }) => {
 
   // Réinitialiser le timer quand on change de question
   useEffect(() => {
-    setTimeLeft(31);
+    setTimeLeft(11);
     setAnswerTime(0);
   }, [currentQuestionIndex]);
 
@@ -54,7 +54,7 @@ const Quiz = ({ questions, studentName, onComplete }) => {
         userAnswer: 'Aucune réponse',
         correctAnswer: currentQuestion.correctAnswer,
         isCorrect: false,
-        timeSpent: 31
+        timeSpent: 11
       };
       
       setAnswers([...answers, answerRecord]);
